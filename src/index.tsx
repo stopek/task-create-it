2eaeded7e8324fca9e554e66ed73535f62d0cd3b
stopek <stopek.pawel@gmail.com>
2022-04-04 12:09:26 +0200

    feat: added material ui

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
